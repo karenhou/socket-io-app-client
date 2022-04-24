1a12fdd9d23ca1e0189a65e5118958ad39d8c50b
Karen Hou <ymcl760728@gmail.com>
2022-04-24 15:26:41 +0800

    [Feat] add socketIO init components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import GuessingGame from "./components/GuessingGame";
+const socket = io.connect("http://localhost:3001");
+
+const tempObj = {
+  guessCount: 1,
+  roomId: "111",
+  socketId: "EuNtGX8vbkl4l3jFAAAJ",
+  timestamp: 1650778057221,
+};
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [msgReceived, setMsgReceived] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [guessNum, setGuessNum] = useState("");
+  const [chartInfo, setChartInfo] = useState("");
+
+  const sendMessage = () => {
+    console.log("messge clicked");
+    socket.emit("send_message", { message, roomId });
+  };
+
+  const handleJoinRoom = () => {
+    socket.emit("join_room", { roomId });
+  };
+  const handleLeaveRoom = () => {
+    socket.emit("leave_room", { roomId });
+  };
+  const handleStartGame = () => {
+    socket.emit("start_game", { roomId });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMsgReceived(data.message);
+    });
+    socket.on("guessingNum", (data) => {
+      console.log("guessingNum", data);
+      setGuessNum(data.guessNum);
+    });
+
+    socket.on("game_result", (data) => {
+      console.log("game_result", data, new Date().getTime());
+
+      setChartInfo((old) => {
+        console.log("old ", old, data);
+
+        let mySet = new Set([...old, data]);
+
+        return [...mySet];
+      });
+    });
+  }, [socket]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          placeholder="Message..."
+          type="text"
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+        />
+        <button onClick={() => handleJoinRoom()}>Join Room</button>
+        <button onClick={() => handleLeaveRoom()}>Leave Room</button>
+      </div>
+
+      <div>
+        <input
+          placeholder="Message..."
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={() => sendMessage()}>Send msg</button>
+      </div>
+
+      <button onClick={() => handleStartGame()}>Start</button>
+
+      <h1>Message:</h1>
+      {msgReceived}
+      {chartInfo &&
+        chartInfo.map((item) => (
+          <div
+            key={
+              item.sockeId
+            }>{`socketId: ${item.socketId}, used: ${item.guessCount} time to guess`}</div>
+        ))}
+
+      {/* <div>{guessNum}</div> */}
+      {guessNum && (
+        <GuessingGame resultNum={guessNum} socket={socket} roomId={roomId} />
+      )}
     </div>
   );
 }
---
+++ src/components/GuessingGame.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+
+const generateRandomNum = () => {
+  let arr = new Set();
+  while (arr.size !== 4) {
+    let ranNum = Math.floor(Math.random() * 9);
+    arr.add(ranNum);
+  }
+  return [...arr];
+};
+
+const compareResult = (userGuess, answer) => {
+  let rightNumCount = 0; //for B
+  let rightAnswerCount = 0; //for A
+
+  for (let i = 0; i < answer.length; i++) {
+    if (userGuess[i] === answer[i]) {
+      rightAnswerCount++;
+    } else {
+      if (answer.includes(userGuess[i])) {
+        rightNumCount++;
+      }
+    }
+  }
+
+  return [rightAnswerCount, rightNumCount];
+};
+
+const checkEndGame = (result) => {
+  return result[0] === 4;
+};
+
+const GuessingGame = ({ resultNum, socket, roomId }) => {
+  const [randomNum, setRandomNum] = useState([]);
+  const [guessRecord, setGuessRecord] = useState([]);
+  const [inputGuess, setInputGuess] = useState("");
+  const [hasWon, setHasWon] = useState(0);
+
+  useEffect(() => {
+    setRandomNum(resultNum);
+  }, []);
+
+  const handleGuess = (guess) => {
+    const userGuess = guess.split("").map((x) => parseInt(x));
+    // console.log("useGuess", userGuess);
+
+    let result = compareResult(userGuess, randomNum);
+
+    // console.log("result ", result);
+
+    let hasWon = checkEndGame(result);
+
+    // console.log("result ", hasWon);
+    if (!hasWon) {
+      setGuessRecord((oldRecord) => {
+        if (oldRecord.length === 9) {
+          console.log("oldRecord", oldRecord.length);
+          setHasWon(2);
+          return [];
+        } else {
+          return [...oldRecord, `${inputGuess} ${result[0]}A ${result[1]}B`];
+        }
+      });
+    } else {
+      let resultObj = {
+        roomId: roomId,
+        guessCount: 1,
+        socketId: socket.id,
+      };
+
+      if (guessRecord.length > 0) {
+        resultObj.guessCount = guessRecord.length;
+      }
+      socket.emit("end_game", resultObj);
+      setGuessRecord([]);
+      setHasWon(1); //won
+    }
+
+    setInputGuess("");
+  };
+
+  const handleResetGame = () => {
+    setRandomNum(generateRandomNum());
+    setInputGuess("");
+    setGuessRecord([]);
+    setHasWon(0);
+  };
+
+  return (
+    <div className="text-center">
+      <h2 className="font-bold text-2xl text-center mt-2 mb-3">
+        Guessing Game
+      </h2>
+      <div className="text-center border-2 border-slate-300 w-16 h-8 rounded-md text-white hover:text-red-200 mx-auto">
+        {randomNum}
+      </div>
+      <div>
+        <input
+          type="text"
+          className="border-2 rounded p-2 text-black mt-2"
+          value={inputGuess}
+          onChange={(e) => setInputGuess(e.target.value)}
+        />
+        <button
+          className="ml-2 rounded bg-green-500 text-white p-2 disabled:hover:cursor-not-allowed"
+          disabled={inputGuess.length === 0}
+          onClick={() => handleGuess(inputGuess)}>
+          Guess
+        </button>
+        <button
+          className="ml-2 rounded bg-orange-500 text-white p-2"
+          onClick={() => handleResetGame()}>
+          Reset
+        </button>
+      </div>
+
+      <h4>Guesses Log, Guess Count:{guessRecord.length}</h4>
+      <span className="text-gray-400">
+        You have up to 10 guess before game over
+      </span>
+
+      {hasWon === 2 ? (
+        <div>You lose, answer:{randomNum}</div>
+      ) : (
+        <>
+          {hasWon === 1 ? (
+            <div>You won</div>
+          ) : (
+            <ul>
+              {guessRecord.map((guess, index) => {
+                return <li key={index}>{guess}</li>;
+              })}
+            </ul>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default GuessingGame;
